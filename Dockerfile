d509ee420a5bb6b046296838c84b77a44516d751
Ugo <ugo.mignon@iex.ec>
2025-03-27 11:03:55 +0100

    fix: Anvil setup

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM ghcr.io/foundry-rs/foundry:stable
-
-COPY start-anvil.sh ./start-anvil.sh
-RUN chmod +x ./start-anvil.sh
-
-ENTRYPOINT ["./start-anvil.sh"]
--- test/work.test.js
+++ test/work.test.js
@@ -1,3 +1,16 @@
-import {expect, test} from "vitest";
+import { expect, test } from "vitest";
 
-// TODO: Implement the tests
+test("Anvil is running", async () => {
+    const response = await fetch("http://127.0.0.1:8545", { // Change localhost to 127.0.0.1
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            jsonrpc: "2.0",
+            id: 1,
+            method: "eth_chainId",
+            params: []
+        })
+    });
+    const data = await response.json();
+    expect(data).toHaveProperty("result");
+});
